bd312842d05d68236b9a90594a8e697e7bc571f4
zhourx <zhourx1129@qq.com>
2022-07-15 23:38:39 +0800

    杨辉三角形

--- Go/leetcode118/main.go
+++ Go/leetcode118/main.go
@@ -10,12 +10,11 @@ func generate(numRows int) [][]int {
 	arr := make([][]int,numRows)
 	for i := 0; i < numRows; i++ {
 		arr[i] = make([]int, i+1)
-		for j := 0; j <= i; j++ {
-			if j == 0 || j == i {
-				arr[i][j] = 1
-			} else {
-				arr[i][j] = arr[i-1][j-1] + arr[i-1][j]
-			}
+		arr[i][0]=1
+		arr[i][i] = 1
+		for j := 1; j <= i/2; j++ {
+			arr[i][j] = arr[i-1][j-1] + arr[i-1][j]
+			arr[i][i-j] = arr[i][j]
 		}
 	}
 	return arr
