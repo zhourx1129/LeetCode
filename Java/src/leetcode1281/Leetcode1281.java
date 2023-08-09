970974484d7728451e4dd2ec21cbb3d5c8ef9ccb
zhourx <zhourx1129@qq.com>
2023-08-09 12:55:01 +0800

    leetcode1281[200~整数的各位积和之差

---
+++ Java/src/leetcode1281/Leetcode1281.java
@@ -0,0 +1,19 @@
+
+// 给你一个整数 n，请你帮忙计算并返回该整数「各位数字之积」与「各位数字之和」的差。
+
+public class Leetcode1281{
+    Solution s = new Solution();
+    int result = s.subtractProductAndSum(234);
+    System.out.println(result);
+}
+class Solution {
+    public int subtractProductAndSum(int n) {
+       int sum=0,product=1;
+        while(n!=0){
+            product *= n%10;
+            sum += n%10;
+            n/=10;
+        }
+        return product-sum;
+    }
+}
