a162d13e6490272d7e365a210117ab7e309983e4
zhourx <zhourx1129@qq.com>
2023-09-11 17:28:26 +0800

    24. 两两交换链表中的节点

--- Java/src/leetcode24/swapPairs.java
+++ Java/src/leetcode24/swapPairs.java
@@ -9,7 +9,7 @@ class ListNode {
     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
 }
 class Solution {
-    public ListNode swapPairs(ListNode head) {
+   /* public ListNode swapPairs(ListNode head) {
         if (head==null || head.next==null){
             return head;
         }
@@ -18,5 +18,31 @@ class Solution {
         two.next = head;
         head.next = swapPairs(three);
         return two;
+    }*/
+/*    public ListNode swapPairs(ListNode head) {
+        //如果当前节点和当前节点的下一节点是空，则直接返回当前节点
+        if (head==null || head.next==null)
+            return head;
+        //定义当前节点的下一节点
+        ListNode nextNode = head.next;
+        //让当前节点指向下一节点的后续的节点，同时完成后面节点的递归指向
+        head.next = swapPairs(nextNode.next);
+        //当头结点指向“第三个节点”后 之前的第二节点就可以直接指向头结点（现在的第二节点）
+        nextNode.next=head;
+        return nextNode;
+}*/
+
+    public ListNode swapPairs(ListNode head){
+         //如果当前节点和当前节点的下一节点是空，则直接返回当前节点
+        if (head==null || head.next==null)
+            return head;
+        //定义当前节点的下一节点
+        ListNode nextNode = head.next;
+        //让当前节点指向下一节点的后续的节点，同时完成后面节点的递归指向
+        head.next = swapPairs(nextNode.next);
+        //当头结点指向“第三个节点”后 之前的第二节点就可以直接指向头结点（现在的第二节点）
+        nextNode.next=head;
+        return nextNode;
     }
+
 }
